42cd85dddb965411e6e0706c6422239854b365c2
Emmanuel Kissi-Addo <kissi09@outlook.com>
2025-07-26 08:10:58 +0000

    logo_new

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import TodoApp from './components/TodoApp';
 import styles from './styles/App.module.css';
 function App() {
-return (
-<div className={styles.container}>
-<h1 className={styles.title}>Kissi's TODO App</h1>
-<TodoApp />
-</div>
-);
+  return (
+    <div className={styles.container}>
+      <h1>Welcome to Kissi&apos;s Todo App</h1>
+      <TodoApp />
+    </div>
+  );
 }
 export default App;
