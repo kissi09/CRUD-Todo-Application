3a40a0fd2ef4d4c79b8e949d7fd9689cd14be606
Emmanuel Kissi-Addo <kissi09@outlook.com>
2025-07-26 07:59:15 +0000

    Title_Alteration

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -4,7 +4,7 @@ import styles from './styles/App.module.css';
 function App() {
 return (
 <div className={styles.container}>
-<h1 className={styles.title}>Advanced React TODO App</h1>
+<h1 className={styles.title}>Kissi's TODO App</h1>
 <TodoApp />
 </div>
 );
